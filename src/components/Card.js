e036a0cd356df2aeb0dae70d82013109b3866138
Sonu <sonu3660@gmail.com>
2021-03-15 20:20:26 +0530

    Implemented functionality for edit and delete option

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,13 +1,54 @@
 import React from 'react';
 
 import QuestionAnswerIcon from '@material-ui/icons/QuestionAnswer';
+import Button from '@material-ui/core/Button';
+import Paper from '@material-ui/core/Paper';
+import Grid from '@material-ui/core/Grid';
+
 
 class Card extends React.Component{
+
+    onClickDelete = (e)=>{
+        e.preventDefault();
+        this.props.onDelete(this.props.index)
+    }
+
+    onClickEdit = (e)=>{
+        e.preventDefault();
+        this.props.onEdit(this.props.index)
+    }
+
     render(){
-        return (<div>
-            <QuestionAnswerIcon/>
-        </div>)
+        return (
+                    <Grid item xs={4} style={{marginTop:"1%"}} >
+                        <Paper>
+                        <div className="card">
+                            <div className="card-left">
+                                <QuestionAnswerIcon/>
+                            </div>
+                        <div className="card-right">
+                        <div>
+                            <div style={{marginTop:"3%"}}>
+                                <b>{this.props.data.title}</b>
+                                <p>{this.props.data.desc}</p>
+                            </div>
+                            <div className="card-btn">
+                            <Button variant="contained" color="primary" onClick={e=>this.onClickEdit(e)} >
+                                Edit
+                            </Button>
+                            <Button variant="contained" color="primary" onClick={e=>this.onClickDelete(e)}>
+                                Delete
+                            </Button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                </Paper>
+            </Grid>
+
+                )
     }
 }
 
 export default Card;
+
--- src/components/Form.js
+++ src/components/Form.js
@@ -2,30 +2,92 @@ import React from 'react';
 
 import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
+import Grid from '@material-ui/core/Grid';
 
 import Card from './Card';
 
 // import Image from '../assets/background.jpeg';
 
 class Form extends React.Component{
+
+    state = {title:"", desc:"", data:[], edit:false, editIndex: 0};
+
+    onSubmitForm = (e)=>{
+        e.preventDefault();
+        if(!this.state.edit){
+            if(this.state.title.length!==0 && this.state.desc.length!==0 ){
+                let formData = {
+                    title: this.state.title,
+                    desc: this.state.desc,
+                }
+                this.setState({data: [...this.state.data, formData], title:"", desc:"", edit:false});
+            }
+        }else{
+            let arr = [];
+            this.state.data.map((val, index)=>{
+            if(this.state.editIndex !== index){
+                arr.push(val);
+            }else{
+                let formData = {
+                    title: this.state.title,
+                    desc: this.state.desc,
+                }
+                arr.push(formData);
+            }
+        })
+        this.setState({data: arr, title:"", desc:"", edit:false});
+        }
+    }
+
+    // To delete an item for list
+    onDelete = (ind)=>{
+        let arr = [];
+        this.state.data.map((val, index)=>{
+            if(ind !== index){
+                arr.push(val);
+            }
+        })
+        this.setState({data: arr});
+    }
+
+    // To Edit
+    onEdit = (ind)=>{
+        this.setState({edit:true, editIndex: ind})
+        let arr = [];
+        this.state.data.map((val, index)=>{
+            if(ind !== index){
+                arr.push(val);
+            }else{
+                this.setState({title: val.title, desc: val.desc})
+                arr.push(val);
+            }
+        })
+        this.setState({data: arr});
+    }
+
     render(){
+        // console.log(this.state.data);
         return (<div>
             <div className="image">
                 <div className="form">
                     <form >
                     <div style={{marginBottom:"2%"}}>
-                        <TextField  label="Title" variant="outlined" className="title"/>
+                        <TextField  label="Title" variant="outlined" className="title" required onChange={(e)=>this.setState({title: e.target.value})}  value={this.state.title}/>
                     </div>
                     <div style={{height:"11vh"}}>
-                        <TextField  label="Description" variant="outlined" className="description" />
+                        <TextField  label="Description" variant="outlined" className="description" required onChange={(e)=>this.setState({desc: e.target.value})} value={this.state.desc}/>
                     </div>
-                    <Button variant="contained" color="primary" className="btn">
+                    <Button variant="contained" color="primary" className="btn" onClick={e=> this.onSubmitForm(e)} >
                         Submit
                     </Button>
                     </form>
                 </div>
         </div>
-            <Card />
+            <Grid container>
+                {this.state.data.map((val, index)=>{
+                    return <Card data={val} key={index} index={index} onDelete={this.onDelete} onEdit={this.onEdit}/> 
+                })}           
+            </Grid>
         </div>)
     }
 }
